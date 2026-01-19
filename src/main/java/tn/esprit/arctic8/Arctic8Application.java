1a15298d902ff90dfc84f5a6d3428940e943de08
Saifeddine Jaballah <saifedine.jaballah@gmail.com>
2026-01-19 11:50:11 +0100

    Initialize Spring Boot project with Maven wrapper, configuration files, and basic application structure

---
+++ src/main/java/tn/esprit/arctic8/Arctic8Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.arctic8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arctic8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arctic8Application.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/arctic8/Arctic8ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.arctic8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arctic8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

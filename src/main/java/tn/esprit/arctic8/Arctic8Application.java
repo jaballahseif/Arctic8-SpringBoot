bedb7d1a8820653ad9b6a7182a5d471899aef537
Saifeddine Jaballah <saifedine.jaballah@gmail.com>
2026-01-19 11:50:20 +0100

    Refactor code formatting in pom.xml, Arctic8Application.java, and Arctic8ApplicationTests.java for consistency

--- src/main/java/tn/esprit/arctic8/Arctic8Application.java
+++ src/main/java/tn/esprit/arctic8/Arctic8Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Arctic8Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Arctic8Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Arctic8Application.class, args);
+    }
 
 }
